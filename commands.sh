4f79ba92ef0bc20ffc7073d0eb782a10c876efcf
MithilRocks <mithilbhoras@yahoo.co.in>
2023-08-07 16:10:12 +0530

    New file changes

--- commands.sh
+++ commands.sh
@@ -12,10 +12,10 @@ export FABRIC_CFG_PATH=${PWD}/configtx
 configtxgen -profile DrugCounterfeitOrdererGenesis -channelID system-channel -outputBlock ./system-genesis-block/genesis.block
 
 export IMAGE_TAG=latest
-docker-compose -f docker/docker-compose-certnet.yaml -f docker/docker-compose-ca.yaml up -d
+docker-compose -f docker/docker-compose-pharmachannel.yaml -f docker/docker-compose-ca.yaml up -d
 docker ps -a
 
-configtxgen -profile DrugCounterfeitChannel -outputCreateChannelTx ./channel-artifacts/certnet.tx -channelID certnet
+configtxgen -profile DrugCounterfeitChannel -outputCreateChannelTx ./channel-artifacts/pharmachannel.tx -channelID pharmachannel
 
 export CORE_PEER_TLS_ENABLED=true
 export FABRIC_CFG_PATH=$PWD/../config/
@@ -24,7 +24,7 @@ export CORE_PEER_LOCALMSPID="consumerMSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/consumer.drugcounterfeit.com/peers/peer0.consumer.drugcounterfeit.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/consumer.drugcounterfeit.com/users/Admin@consumer.drugcounterfeit.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
-peer channel create -o localhost:7050 -c certnet --ordererTLSHostnameOverride orderer.drugcounterfeit.com -f ./channel-artifacts/certnet.tx --outputBlock "./channel-artifacts/certnet.block" --tls --cafile $ORDERER_CA
+peer channel create -o localhost:7050 -c pharmachannel --ordererTLSHostnameOverride orderer.drugcounterfeit.com -f ./channel-artifacts/pharmachannel.tx --outputBlock "./channel-artifacts/pharmachannel.block" --tls --cafile $ORDERER_CA
 
-export BLOCKFILE="./channel-artifacts/certnet.block"
+export BLOCKFILE="./channel-artifacts/pharmachannel.block"
 peer channel join -b $BLOCKFILE
