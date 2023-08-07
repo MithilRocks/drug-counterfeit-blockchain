ffd680b2acd913f9387e8f0cb7d158fa2bef6b79
MithilRocks <mithilbhoras@yahoo.co.in>
2023-08-08 04:05:44 +0530

    Reworked docker and configtx files

--- commands.sh
+++ commands.sh
@@ -28,3 +28,22 @@ peer channel create -o localhost:7050 -c pharmachannel --ordererTLSHostnameOverr
 
 export BLOCKFILE="./channel-artifacts/pharmachannel.block"
 peer channel join -b $BLOCKFILE
+
+# --- NEW COMMANDS ---
+export PATH=${PWD}/../bin:$PATH
+
+export FABRIC_CFG_PATH=${PWD}/configtx
+configtxgen -profile DrugCounterfeitOrdererGenesis -channelID pharmachannel -outputBlock ./channel-artifacts/pharmachannel.block
+
+export FABRIC_CFG_PATH=$PWD/../config/
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/drugcounterfeit.com/orderers/orderer.drugcounterfeit.com/msp/tlscacerts/tlsca.drugcounterfeit.com-cert.pem
+export ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/drugcounterfeit.com/orderers/orderer.drugcounterfeit.com/tls/server.crt
+export ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/drugcounterfeit.com/orderers/orderer.drugcounterfeit.com/tls/server.key
+osnadmin channel join --channelID pharmachannel  --config-block ./channel-artifacts/pharmachannel.block -o localhost:7053 --ca-file $ORDERER_CA --client-cert $ADMIN_TLS_SIGN_CERT --client-key $ADMIN_TLS_PRIVATE_KEY
+
+export CORE_PEER_LOCALMSPID="consumerMSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/consumer.drugcounterfeit.com/peers/peer0.consumer.drugcounterfeit.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/consumer.drugcounterfeit.com/users/Admin@consumer.drugcounterfeit.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+export BLOCKFILE="./channel-artifacts/pharmachannel.block"
+peer channel join -b $BLOCKFILE
